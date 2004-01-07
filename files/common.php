5fa807074824f7ef16c66bfb0c92a8b6ac54d242
Murphy <eqom14@users.sourceforge.net>
2004-01-07 00:20:53 +0000

    New logo contest.

--- files/common.php
+++ files/common.php
@@ -12,8 +12,8 @@
 if (!defined(BASEDIR)) {
   define(BASEDIR, "/home/groups/g/gt/gtk-gnutella/htdocs/files/");
 }
-define(NEWSMIN, 21);
-define(NEWSMAX, 32);
+define(NEWSMIN, 22);
+define(NEWSMAX, 33);
 
 define(BASEURL, "$PHP_SELF");
 define(GENDIR,  "general/");
