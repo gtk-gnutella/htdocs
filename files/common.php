f2b56a2537a745640225898b094efee91496f50c
Murphy <eqom14@users.sourceforge.net>
2004-01-08 00:10:43 +0000

    Added pointer to my contributed packages.

--- files/common.php
+++ files/common.php
@@ -12,8 +12,8 @@
 if (!defined(BASEDIR)) {
   define(BASEDIR, "/home/groups/g/gt/gtk-gnutella/htdocs/files/");
 }
-define(NEWSMIN, 22);
-define(NEWSMAX, 33);
+define(NEWSMIN, 23);
+define(NEWSMAX, 34);
 
 define(BASEURL, "$PHP_SELF");
 define(GENDIR,  "general/");
