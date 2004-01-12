88ce260fd4e99ced80571f8564268200eee352f6
Richard Eckart de Castilho <richard.eckart@googlemail.com>
2004-01-12 17:47:03 +0000

    fixed typo

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ include("files/common.php");
 					</div>
 					<div>
 						<a href="http://sourceforge.net">
-							<img src="http://sourceforge.net/sflogo.php?group_id=4467&type=1"
+							<img src="http://sourceforge.net/sflogo.php?group_id=4467&amp;type=1"
 								width="88" height="31" border="0" alt="SourceForge.net Logo"/></a>
 					</div>
 					<div>
