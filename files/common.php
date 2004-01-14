67547567c6cc68eadb749242c81f520dd51effe5
Murphy <eqom14@users.sourceforge.net>
2004-01-14 10:00:05 +0000

    New function to automatically include the NEWSNUM most recent news items on the front page, so we don't have to adjust NEWSMAX and NEWSMIN manually with each new item.

--- files/common.php
+++ files/common.php
@@ -8,16 +8,19 @@
  * general configuration variables are in here
  *
  */
+ 
+/* Uncomment this for debugging */
+/* error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); */
 
 if (!defined(BASEDIR)) {
   define(BASEDIR, "/home/groups/g/gt/gtk-gnutella/htdocs/files/");
 }
-define(NEWSMIN, 24);
-define(NEWSMAX, 35);
 
-define(BASEURL, "$PHP_SELF");
-define(GENDIR,  "general/");
-define(LANG,    getlang());
+/* NEWSNUM is the number of news items that appear on the front page. */
+define("NEWSNUM", 7);
+define("BASEURL", "$PHP_SELF");
+define("GENDIR",  "general/");
+define("LANG",    getlang());
 
 include(BASEDIR . "VERSION");
 
@@ -37,14 +40,16 @@ $pages = array(
   "searching_by_hash",
   "developers_howto",
   "bugreport_howto",
-  "release_history"
+  "release_history",
+	"news_header",
+	"news_bridge",
+	"news_old_header"
 );
 
 /* ** hack ** fscking damn php won't use LANG in header() ** hack ** */
-$ru = $CHARSET[ru] = 'koi8-r';
+$ru = $CHARSET["ru"] = 'koi8-r';
 if (isset($CHARSET[LANG]))
   header("Content-Type: text/html; charset=$ru");
-
 
 /* getlang sub - check which language the visitor wants */
 function getlang() {
@@ -84,7 +89,7 @@ function icecontent($content) {
  * checks whether a file is available in the desired language
  */
 function iceinclude($file, $box) {
-  global $page; /* so the included files know about it */
+  global $page, $news_items; /* so the included files know about it */
   $incfile = BASEDIR . LANG . "/$file";
   if (!file_exists($incfile)) {
     $incfile = BASEDIR . "en/$file";
@@ -122,7 +127,11 @@ function maincontent() {
     )
   ) {
     icecontent("$page");
-  } else {
+  } 
+	elseif($page == "news_old") {
+		iceinclude("news_old", 0);
+	}
+	else {
     iceinclude("news", 0);
   }
 }
@@ -135,3 +144,24 @@ function maincontent() {
  function sf_bug($number) {
    print("<a href=\"http://sourceforge.net/tracker/index.php?func=detail&amp;aid=$number&amp;group_id=4467&amp;atid=104467\">#$number</a>");
  }
+ 
+/*
+ * newsfiles:
+ *
+ * Fill the array $news_items with the news_nn filenames,
+ * sort them in natural order, and then reverse the order.
+ * Then we can suck out however many we want.
+ */
+function newsfiles() {
+	global $news_items;
+	$handle = opendir(BASEDIR . "en");
+	while (false !== ($file = readdir($handle))) {
+		if (ereg("news_([0-9]{1,3})", $file)) {
+			$news_items[] = $file;
+		}
+	}
+	closedir($handle);
+	usort($news_items, "strnatcmp");
+	$news_items = array_reverse($news_items);
+}
+
