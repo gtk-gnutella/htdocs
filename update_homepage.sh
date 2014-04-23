e36ca4b42e2763c16a9519b1cb49d39d29d73f5d
Raphael Manfredi <Raphael_Manfredi@pobox.com>
2014-04-23 10:00:14 +0200

    Use verbose rsync to monitor transfer progress.

--- update_homepage.sh
+++ update_homepage.sh
@@ -5,7 +5,8 @@ then
 	exit	
 fi
 
-git archive --format=tar HEAD | tar -t  | rsync --files-from=- -lv --delete --chmod=Dug+x,g-w,Fg-w,o-w  . $1,gtk-gnutella@web.sourceforge.net:/home/project-web/gtk-gnutella/htdocs
-
-
+git archive --format=tar HEAD | tar -t  | \
+	rsync --verbose --files-from=- -lv --delete \
+	--chmod=Dug+x,g-w,Fg-w,o-w . \
+	$1,gtk-gnutella@web.sourceforge.net:/home/project-web/gtk-gnutella/htdocs
 
